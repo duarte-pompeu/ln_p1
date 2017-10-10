86a6749d3be91100c549e44f622c6a8dbdb3a727
Your Name <you@example.com>
2017-10-10 18:29:07 +0100

    implement transducers 2 and 3

---
+++ transdutores/2/compile.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/fish
+
+python3 ../../material_apoio/scripts/compact2fst.py transdutor2_compact.txt > transdutor2.txt; 
+and fst transdutor2.txt;
+
---
+++ transdutores/3/compile.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/fish
+
+python3 ../../material_apoio/scripts/compact2fst.py transdutor3_compact.txt > transdutor3.txt; 
+and fst transdutor3.txt;
+
