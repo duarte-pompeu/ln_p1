095597771293c2be4eba361861038a3620c854b7
Your Name <you@example.com>
2017-10-10 15:58:41 +0100

    change folder names

--- transdutores/1/compile.fish
+++ transdutores/1/compile.fish
@@ -1,3 +1,12 @@
 #!/usr/bin/fish
 
-fst romanos.txt
+python3 ../../material_apoio/scripts/compact2fst.py palavras_compact.txt > palavras.txt; 
+and fst palavras.txt;
+and fst underscore.txt;
+and fstconcat palavras.fst underscore.fst > palavras_.fst;
+and fstconcat ../romanos/romanos.fst underscore.fst > romanos_.fst
+and fstunion romanos_.fst palavras_.fst > uniao.fst;
+and fstd uniao.fst;
+and fstclosure uniao.fst > closure.fst
+and fstd closure.fst
+
---
+++ transdutores/romanos/compile.fish
@@ -0,0 +1,3 @@
+#!/usr/bin/fish
+
+fst romanos.txt
