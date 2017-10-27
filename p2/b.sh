85295b16369c82aad4112489a2243c65c659fe93
Your Name <you@example.com>
2017-10-27 22:47:14 +0100

    finish script for 1.b

--- p2/b.sh
+++ p2/b.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-#cat <&0\ | grep -P "n-é-verbo.*\t" 
-#~ cat <&0 | grep -P "[\t].*\bvir\b.*\bvir\b.*"
-#~ cat <&0 | grep -P "#.*\t"
-#cat <&0 | grep -P "\?.*\t"
-#cat <&0 | grep -P "\?.*\t"
-# trocar palavra por lema
-cat <&0 | cut -f 2
+# testar se frases não desejadas estão a ser filtradas bem
+mkdir teste
+cat $1 | grep -P "n-é-verbo.*\t" > teste/n_verbo.txt
+cat $1 | grep -P "[\t].*\bvir\b.*\bvir\b.*" > teste/2x.txt
+cat $1 | grep -P "#.*\t" > teste/erros.txt
+cat $1 | grep -P "\?.*\t" > teste/duvidas.txt
 
+# filtrar frases não desejadas
+FINAL=$(echo $1 | cut -d . -f 1).final
+cat $1 | grep -vP "n-é-verbo.*\t" \
+| grep -vP "[\t].*\bvir\b.*\bvir\b.*" \
+| grep -vP "#.*\t" \
+| grep -vP "\?.*\t" \
+> $FINAL
+
+# alterar verbo para lema no inicio da frase
+# formato da frase: (lema)\t(texto)(vir)(texto)
+sed -E -i 's/(.*)\t(.*)(vir)(.*)/\2\1\4/g' $FINAL
