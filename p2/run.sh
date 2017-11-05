faa7d334963aac75fbb0bc6b08ce1a21d3af3d50
Your Name <you@example.com>
2017-11-05 23:22:09 +0000

    finish

--- p2/run.sh
+++ p2/run.sh
@@ -3,14 +3,15 @@
 # TAREFA 1
 # testar se frases não desejadas estão a ser filtradas bem
 mkdir teste
-cat $1 | grep -P "n-é-verbo.*\t" > teste/n_verbo.txt
-cat $1 | grep -P "[\t].*\bvir\b.*\bvir\b.*" > teste/2x.txt
-cat $1 | grep -P "#.*\t" > teste/erros.txt
-cat $1 | grep -P "\?.*\t" > teste/duvidas.txt
+input="virAnotado.final"
+cat $input | grep -P "n-é-verbo.*\t" > teste/n_verbo.txt
+cat $input | grep -P "[\t].*\bvir\b.*\bvir\b.*" > teste/2x.txt
+cat $input | grep -P "#.*\t" > teste/erros.txt
+cat $input | grep -P "\?.*\t" > teste/duvidas.txt
 
 # filtrar frases não desejadas
-FINAL=$(echo $1 | cut -d . -f 1).final
-cat $1 | grep -vP "n-é-verbo.*\t" \
+FINAL=$(echo $input | cut -d . -f 1).final
+cat $input | grep -vP "n-é-verbo.*\t" \
 | grep -vP "[\t].*\bvir\b.*\bvir\b.*" \
 | grep -vP "#.*\t" \
 | grep -vP "\?.*\t" \
